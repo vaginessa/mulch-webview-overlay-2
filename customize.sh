04cb0262f70f4cdc89602d32bdc72955ad52aed6
@LeanHijosdesusMadres <105339980+LeanxModulostk@users.noreply.github.com>
2023-08-14 12:57:07 -0500

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,5 @@
+##########################################################################################
+
+nohup am start -a android.intent.action.VIEW -d https://t.me/modulostk >/dev/null 2>&1 &
+
+################[t.me/modulostk]###############
